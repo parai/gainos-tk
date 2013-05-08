085c08a3ccd4d63e8559583db1a537821100bc81
parai(wangfan) <parai@foxmail.com>
2013-05-08 21:47:55 +0800

    init atsam3s

--- bin/at91sam3s4c/main.c
+++ bin/at91sam3s4c/main.c
@@ -1,31 +1,14 @@
-#include "chip.h"
+#include "board.h"
 #include <stdio.h>
 #include "Os.h"
 
-#ifdef __GNUC__
-  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-     set to 'Yes') calls __io_putchar() */
-int __io_putchar(int ch)
-#else
-int fputc(int ch,FILE *f)
-#endif /* __GNUC__ */
-{
-  return ch;
-}
-void Usart_Init(void)
-{
-}
-void SystemClockInit(void)
-{
-}
 #define BOOT_MESSAGE    \
 "GaInOS-TK is really a copy of uTenux.\r\n"  \
 "Author: parai\r\n"   \
 "Email:parai@foxmail.com"
 int main()
 {
-    SystemClockInit();
-    Usart_Init();
+    CLOCK_SetConfig(2);
     printf("\r\nStart OS.%s\r\n",BOOT_MESSAGE);
     StartOS(OSDEFAULTAPPMODE);
     return 0;
