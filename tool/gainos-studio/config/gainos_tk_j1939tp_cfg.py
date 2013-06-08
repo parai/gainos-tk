fdc78d14e64333c69c98a02b32106dbd887b2169
parai <parai@foxmail.com>
2013-06-08 22:01:52 +0800

    up GUI J1939Tp

--- tool/gainos-studio/config/gainos_tk_j1939tp_cfg.py
+++ tool/gainos-studio/config/gainos_tk_j1939tp_cfg.py
@@ -7,7 +7,6 @@
  */
 """
 
-
 from PyQt4.QtGui import QDialog
 from PyQt4.QtCore import pyqtSignature
 from PyQt4.QtGui import QTreeWidgetItem, QMessageBox
@@ -16,10 +15,92 @@ from PyQt4.QtCore import QStringList,QString
 import os, sys
 from Common import *
 import xml.etree.ElementTree as ET
-        
+class J1939TpGeneral():
+    def __init__(self):
+        self.DevError = True;
+        self.PacketBlock = 5;
+        self.MainFunctionPeriod = 4;
+        self.TxConfirmTimeout = 100;
+    def save(self, root):
+        nd = ET.Element('J1939TpGeneral');
+        nd.attrib['DevError'] = str(self.DevError);
+        nd.attrib['PacketBlock'] = str(self.PacketBlock);
+        nd.attrib['MainFunctionPeriod'] = str(self.MainFunctionPeriod);
+        nd.attrib['TxConfirmTimeout'] = str(self.TxConfirmTimeout);
+        root.append(nd);
+    def parse(self, nd):
+        self.DevError = bool(nd.attrib['DevError']);
+        self.PacketBlock = int(nd.attrib['PacketBlock']);
+        self.MainFunctionPeriod = int(nd.attrib['MainFunctionPeriod']);
+        self.TxConfirmTimeout = int(nd.attrib['TxConfirmTimeout']);
+class J1939TpPgs():
+    def __init__(self, name):
+        self.name = name;
+    def save(self, root):
+        nd = ET.Element('J1939TpPgs');
+        nd.attrib['name'] = str(self.name);
+        root.append(nd);
+    def parse(self, nd):
+        self.name = nd.attrib['name'];
+class J1939TpChannel():
+    def __init__(self, name):
+        self.name = name;
+        self.Protocol = ''
+        self.DtNPdu = ''
+        self.CmNPdu = ''
+        self.FcNPdu = ''
+        self.Direction = ''
+        self.PgsList = []
+    def save(self, root):
+        nd = ET.Element('J1939TpChannel');
+        nd.attrib['name'] = str(self.name);
+        nd.attrib['DtNPdu'] = str(self.DtNPdu);
+        nd.attrib['CmNPdu'] = str(self.CmNPdu);
+        nd.attrib['FcNPdu'] = str(self.FcNPdu);
+        nd.attrib['Direction'] = str(self.Direction);
+        nd2 = ET.Element('PgsList');
+        for obj in self.PgsList:
+            obj.save(nd2);
+        nd.append(nd2);
+        root.append(nd);
+    def parse(self, nd):
+        self.name = nd.attrib['name'];
+        self.DtNPdu = str(nd.attrib['DtNPdu']);
+        self.CmNPdu = str(nd.attrib['CmNPdu']);
+        self.FcNPdu = str(nd.attrib['FcNPdu']);
+        self.Direction = str(nd.attrib['Direction']);
+        for nd2 in nd.find('PgsList'):
+            obj = J1939TpPgs('');
+            obj.parse(nd2);
+            self.PgsList.append(obj);
+class gainos_tk_j1939tp_obj():
+    def __init__(self):
+        self.general = J1939TpGeneral();
+        self.rxChannelList = [];
+        self.txChannelList = [];
+    def save(self, root):
+        self.general.save(root);
+        nd = ET.Element('rxChannelList');
+        for obj in self.rxChannelList:
+            obj.save(nd);
+        root.append(nd);
+        nd = ET.Element('txChannelList');
+        for obj in self.txChannelList:
+            obj.save(nd);
+        root.append(nd);
+    def parse(self, root):
+        self.general.parse(root.find('J1939TpGeneral'));
+        for nd in root.find('rxChannelList'):
+            obj = J1939TpChannel('');
+            obj.parse(nd);
+            self.rxChannelList.append(obj);
+        for nd in root.find('txChannelList'):
+            obj = J1939TpChannel('');
+            obj.parse(nd);
+            self.txChannelList.append(obj);
 class gainos_tk_j1939tp_cfg():
     def __init__(self, chip=None):
-        self.cfg=None;
+        self.cfg=gainos_tk_j1939tp_obj();
 
     def toString(self):
         str='J1939Tp!\n';
@@ -32,10 +113,10 @@ class gainos_tk_j1939tp_cfg():
         self.dlg.show();
    
     def save(self, root):
-        return
+        self.cfg.save(root);
     
     def parse(self, root):
-        return
+        self.cfg.parse(root);
 
     def gen(self, path):
         return;
--- tool/gainos-studio/ui/classes/cd_j1939tp.py
+++ tool/gainos-studio/ui/classes/cd_j1939tp.py
@@ -6,9 +6,10 @@ Module implementing cd_j1939tp.
 
 from PyQt4.QtGui import QDialog
 from PyQt4.QtCore import pyqtSignature
-
+from PyQt4.QtCore import QStringList,QString
 from Ui_cd_j1939tp import Ui_cd_j1939tp
-
+from PyQt4.QtGui import QTreeWidgetItem, QMessageBox
+from gainos_tk_j1939tp_cfg import *
 class cd_j1939tp(QDialog, Ui_cd_j1939tp):
     """
     Class documentation goes here.
@@ -20,46 +21,83 @@ class cd_j1939tp(QDialog, Ui_cd_j1939tp):
         QDialog.__init__(self, parent)
         self.setupUi(self)
         self.setWindowTitle(title);
-    
-    @pyqtSignature("int")
-    def on_spbxMainFncPeriod_valueChanged(self, p0):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
-    
-    @pyqtSignature("int")
-    def on_spbxCfTmo_valueChanged(self, p0):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
-    
-    @pyqtSignature("")
-    def on_cbxDevError_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
-    
-    @pyqtSignature("int")
-    def on_spbxPktBlk_valueChanged(self, p0):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
-    
+        self.cfg = cfg;
+        self.fileInd = fileInd;
+        self.curtree = None;
+        self.curobj = None;
+        self.initGui();
+    def initGeneral(self):
+        self.cbxDevError.setChecked(self.cfg.general.DevError);
+        self.spbxPktBlk.setValue(self.cfg.general.PacketBlock);
+        self.spbxMainFncPeriod.setValue(self.cfg.general.MainFunctionPeriod);
+        self.spbxCfTmo.setValue(self.cfg.general.TxConfirmTimeout);
+    def reloadTreeChannel(self, index, list):
+        tree=self.trJ1939Tp.topLevelItem(index);
+        for index in range(0, tree.childCount()):
+            temp=tree.takeChild(0);
+            del temp;
+        for obj in list:
+            item=QTreeWidgetItem(tree,QStringList(obj.name));
+            for obj2 in obj.PgsList:
+                item2 = QTreeWidgetItem(item,QStringList(obj2.name));
+    def initGui(self):
+        self.initGeneral();
+        self.reloadTreeChannel(0, self.cfg.rxChannelList)
+        self.reloadTreeChannel(1, self.cfg.txChannelList)
+    def refreshButton(self):
+        if(self.curtree==None):
+            return;
+        trname = self.curtree.text(0);
+        if(trname=='RxChannel'):
+            self.btn1.setText('Add Rx Channel');
+            self.btn1.setDisabled(False);
+            self.btn2.setDisabled(True);
+        elif(trname=='TxChannel'):
+            self.btn1.setText('Add Tx Channel');
+            self.btn1.setDisabled(False);
+            self.btn2.setDisabled(True);
+        elif(self.curtree.parent().text(0)=='RxChannel'):
+            self.btn1.setText('Add Rx Pgs');
+            self.btn1.setDisabled(False);
+            self.btn2.setText('Del Rx Channel');
+            self.btn2.setDisabled(False);
+        elif(self.curtree.parent().text(0)=='TxChannel'):
+            self.btn1.setText('Add Tx Pgs');
+            self.btn1.setDisabled(False);
+            self.btn2.setText('Del Tx Channel');
+            self.btn2.setDisabled(False);
     @pyqtSignature("QTreeWidgetItem*, int")
     def on_trJ1939Tp_itemClicked(self, item, column):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        self.curtree = item;
+        self.refreshButton();
+        
+    #------------- General -------------------------
+    @pyqtSignature("bool")
+    def on_cbxDevError_clicked(self, checked):
+        if(self.cfg.general.DevError != checked):
+            self.cfg.general.DevError = checked;
+            self.fileInd(False);
+    @pyqtSignature("int")
+    def on_spbxPktBlk_valueChanged(self, p0):
+        if(self.cfg.general.PacketBlock != p0):
+            self.cfg.general.PacketBlock = p0;
+            self.fileInd(False);
+    @pyqtSignature("int")
+    def on_spbxMainFncPeriod_valueChanged(self, p0):
+        if(self.cfg.general.MainFunctionPeriod != p0):
+            self.cfg.general.MainFunctionPeriod = p0;
+            self.fileInd(False);
+    @pyqtSignature("int")
+    def on_spbxCfTmo_valueChanged(self, p0):
+        if(self.cfg.general.TxConfirmTimeout != p0):
+            self.cfg.general.TxConfirmTimeout = p0;
+            self.fileInd(False);
+    
+
+    
+
+    
+
     
     @pyqtSignature("QString")
     def on_leTxChlName_textChanged(self, p0):
@@ -188,14 +226,32 @@ class cd_j1939tp(QDialog, Ui_cd_j1939tp):
         """
         # TODO: not implemented yet
         raise NotImplementedError
-    
+    def addRxChannel(self):
+        id = len(self.cfg.rxChannelList);
+        name=QString('vRxChannel%s'%(id));
+        item=QTreeWidgetItem(self.curtree,QStringList(name));
+        obj = J1939TpChannel(name);
+        self.cfg.rxChannelList.append(obj);
+        obj.PgsList.append(J1939TpPgs('vPgs0'));
+        QTreeWidgetItem(item,QStringList('vPgs0'));
+        self.curtree.setExpanded(True);
+    def addTxChannel(self):
+        id = len(self.cfg.txChannelList);
+        name=QString('vTxChannel%s'%(id));
+        item=QTreeWidgetItem(self.curtree,QStringList(name));
+        obj = J1939TpChannel(name);
+        self.cfg.txChannelList.append(obj);
+        obj.PgsList.append(J1939TpPgs('vPgs0'));
+        QTreeWidgetItem(item,QStringList('vPgs0'));
+        self.curtree.setExpanded(True);
     @pyqtSignature("")
     def on_btn1_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        text = self.btn1.text();
+        if(text == 'Add Rx Channel'):
+            self.addRxChannel();
+        elif(text == 'Add Tx Channel'):
+            self.addTxChannel();
+        self.fileInd(False);
     
     @pyqtSignature("")
     def on_btn2_clicked(self):
